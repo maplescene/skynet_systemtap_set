a2e4aa7ad80daa4c46e185834388edbb99417c12
caiyiheng <rangercyh@qq.com>
2019-08-07 10:05:25 +0800

    fix old skynet

--- monitor_skynet_and_gen_svg.sh
+++ monitor_skynet_and_gen_svg.sh
@@ -1,8 +1,8 @@
 #/bin/sh
-if [ "$1" = "" ] | [ "$2" = "" ] | [ "$3" = "" ]; then
-    echo "usage: monitor pid skynet_bin_path service_id_in_decimal"
+if [ "$1" = "" ] | [ "$2" = "" ] | [ "$3" = "" ] | [ "$4" = "" ]; then
+    echo "usage: monitor pid skynet_bin_path service_id_in_decimal seconds"
     exit 1
 fi
-sudo stap -B CONFIG_MODVERSIONS=y -v mini_lua_bt.stp --skip-badvars -x $1 $2 $3 -g --suppress-time-limits -DMAXSTRINGLEN=65536 |tee a.bt
+sudo stap -v mini_lua_bt.stp --skip-badvars -x $1 $2 $3 $4 -g --suppress-time-limits -DMAXSTRINGLEN=65536 |tee a.bt
 
-./flamegraph.pl --width=2400 a.bt >skynet.svg
+./flamegraph.pl --height=30 --width=1200 --fontsize=10 --minwidth=5 a.bt >skynet.svg
